7b52e4c21569b92f86cb76a4816f1e80934ca49f
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-12-08 11:21:44 +0530

    added topic selection and problem counter

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const puppeteer = require("puppeteer");
 const loginLink = " https://www.hackerrank.com/auth/login";
+const email = 'uihfltu@gumaygo.com'
+const password = 'userone'
 let browserOpen = puppeteer.launch({
   headless: false, //makes it visible
   args: ["--start-maximized"], //shows in fullscreen
@@ -22,3 +24,44 @@ return emailIsEntered
 let passwordIsEntered = page.type("input[type = 'password']", password, {delay : 50})
 return passwordIsEntered
 })
+.then(function(){
+    let loginButtonClicked = page.click("button[data-analytics = 'LoginPassword']", {delay : 50})
+    return loginButtonClicked
+}).then(function(){
+    let clickOnAlgoPromise = waitAndClick('.topic-card a[data-attr1 = "algorithms"]', page)
+return clickOnAlgoPromise;
+}).then(function(){
+    let getToWarmUp = waitAndClick('input[value="warmup"]', page)
+    return getToWarmUp;
+}).then(function() {
+let waitfor3Seconds = page.waitFor(3000)
+return waitfor3Seconds
+}).then(function(){
+    let allChallengesPromise = page.$$('.ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled', {delay : 50}) //$$ shorthand for doc.queryselectorall
+        return allChallengesPromise;
+}).then(function(questionArray)
+{
+    console.log('number of questions', questionArray.length);
+})
+
+
+
+
+
+
+
+
+function waitAndClick(selector, cPage) //waits for that element which is available after page gets loaded bbcoz only after that we are able to access those elements 
+{ 
+    return new Promise(function(resolve,reject){
+        let waitForModelPromise = cPage.waitForSelector(selector)
+        waitForModelPromise.then(function(){ 
+            let clickModel = cPage.click(selector)
+           return clickModel
+        }).then(function(){
+            resolve()
+        }).catch(function(err){
+            reject()
+        })
+    })
+}
